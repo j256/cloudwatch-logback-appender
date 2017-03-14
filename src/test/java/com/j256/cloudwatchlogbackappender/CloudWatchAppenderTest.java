3455c3d5f21a4792fca4ab742d6441b6693761b6
Gray Watson <gray.github@mailnull.com>
2017-03-14 13:57:38 -0400

    cp

--- src/test/java/com/j256/cloudwatchlogbackappender/CloudWatchAppenderTest.java
+++ src/test/java/com/j256/cloudwatchlogbackappender/CloudWatchAppenderTest.java
@@ -1,11 +1,13 @@
 package com.j256.cloudwatchlogbackappender;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CloudWatchAppenderTest {
 
+	@Ignore("Integration test to test going to cloudwatch")
 	@Test
 	public void test() throws InterruptedException {
 		Logger logger = LoggerFactory.getLogger(getClass());
