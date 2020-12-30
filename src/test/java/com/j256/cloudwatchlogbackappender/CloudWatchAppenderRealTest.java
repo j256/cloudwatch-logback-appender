ad6146cca4973d70ff7f1ca952e11029ecc0211e
Gray Watson <gray.github@mailnull.com>
2020-12-30 18:55:52 -0500

    cp

--- src/test/java/com/j256/cloudwatchlogbackappender/CloudWatchAppenderRealTest.java
+++ src/test/java/com/j256/cloudwatchlogbackappender/CloudWatchAppenderRealTest.java
@@ -15,6 +15,6 @@ public class CloudWatchAppenderRealTest {
 		logger.error("Here's a throw", new RuntimeException(new Exception("test exception here")));
 		Thread.sleep(1000);
 		logger.info("more stuff");
-		Thread.sleep(10000000);
+		Thread.sleep(10000);
 	}
 }
